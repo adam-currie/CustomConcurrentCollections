8ee07cbe21296111ae45dd1cdf40b50d0a6ce1ea
Adam Currie <Adam.Currie@mail.com>
2020-07-27 14:01:33 -0400

    added high volume queue with a novel buffered enqueue mechanism

---
+++ CustomConcurrentCollections/ConcurrectHighVolumeEnqueueQueue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+
+namespace CustomConcurrentCollections {
+    public class ConcurrectHighVolumeEnqueueQueue<T> : IEnumerable<T> {
+        volatile Node head;
+        volatile Node tail;
+        volatile Node? altTail = null;
+
+        public ConcurrectHighVolumeEnqueueQueue() {
+            head = tail = new Node();
+        }
+
+        public void Enqueue(T item) {
+            Node newNode = new Node(item);
+
+            if (Interlocked.CompareExchange(ref tail.next, newNode, null) == null) {
+                tail = newNode;
+                return;
+            }
+
+            while (true) {
+                Node? cachedAltTail = Interlocked.CompareExchange(ref altTail, newNode, null);
+                if (cachedAltTail == null) {
+                    //we are the root of the alt queue
+                    //so try to reattach to main queue
+                    while (Interlocked.CompareExchange(ref tail.next, newNode, null) != null) {
+                        Thread.Sleep(16);//todo: duration
+                    }
+                    tail = Interlocked.Exchange(ref altTail, null)!;
+                    break;
+                } else if (Interlocked.CompareExchange(ref cachedAltTail.next, newNode, null) == null) {
+                    //cachedAlt queue must be altTail or tail here
+                    if (cachedAltTail == Interlocked.CompareExchange(
+                            location1: ref altTail,
+                            value: newNode,
+                            comparand: cachedAltTail)) {
+                        break;
+                    }
+
+                    //if it's not the altTale then it either is or is about to be the real tale
+                    while (cachedAltTail != Interlocked.CompareExchange(
+                            location1: ref tail,
+                            value: newNode,
+                            comparand: cachedAltTail)) {
+                        Thread.Sleep(1);
+                    }
+                    break;
+                } else {
+                    //retry
+                    Thread.Sleep(1);
+                }
+            }
+            //DEBUG: just here as an alternative to alt queue to compare to naive implementation performance
+            //SpinWait spin = default;
+            //while(true) {
+            //    spin.SpinOnce(-1);
+            //    if (Interlocked.CompareExchange(ref tail.next, newNode, null) == null) {
+            //        tail = newNode;
+            //        return;
+            //    }
+            //}
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            Node? current = head;
+            while ((current = current.next) != null) {
+                yield return current.value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class Node {
+            internal readonly T value;
+            internal volatile Node? next;
+
+            internal Node(T value) {
+                this.value = value;
+            }
+
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+            internal Node() { } //only here for head
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+        }
+
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,5 @@
 using CustomConcurrentCollections;
+using System.Collections.Concurrent;
 using System.Threading;
 using Xunit;
 
@@ -23,7 +24,36 @@ namespace Tests {
 
             foreach (var t in threads) t.Join();
 
-            Assert.Equal(collection.Count, threadCount * insertsPerThread);
+            int[] timesFound = new int[threadCount];
+            int index = 0;
+            foreach (int item in collection) {
+                Assert.True(timesFound[item] < insertsPerThread, "same value inserted too many times: " + item);
+                timesFound[item]++;
+                index++;
+            }
+
+            //checking that we iterated over the correct number of items
+            //if we did and no item was repeated too many times, then all items must have been added the correct number of times
+            Assert.Equal(index, threadCount * insertsPerThread);
+        }
+
+        [Theory]
+        [InlineData(16, 100000)]
+        public void TestConcurrectHighVolumeEnqueueQueue(int threadCount, int insertsPerThread) {
+            var collection = new ConcurrectHighVolumeEnqueueQueue<object>();
+            Thread[] threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++) {
+                Thread thread = new Thread((x)
+                    => {
+                        for (int j = 0; j < insertsPerThread; j++)
+                            collection.Enqueue(x);
+                    });
+                threads[i] = thread;
+                thread.Start(i);
+            }
+
+            foreach (var t in threads) t.Join();
 
             int[] timesFound = new int[threadCount];
             int index = 0;
